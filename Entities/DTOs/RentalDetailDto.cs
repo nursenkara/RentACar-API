66083684f5d7c3de42ea4330d6d2ff18f92fcebd
Nur SENKARA <nsenkara19@gmail.com>
2021-12-02 00:22:50 +0300

    Core katmanına IDto ve Entities katmanına DTOs klasörü eklendi. CarDetailDto ve RentalDetailDto yazıldı .

---
+++ Entities/DTOs/RentalDetailDto.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class RentalDetailDto:IDto
+    {
+        public int RentalId { get; set; }
+        public int CarId { get; set; }
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Model { get; set; }
+        public string BrandName { get; set; }
+        public string ColorName { get; set; }
+        public string ModelYear { get; set; }
+        public string Description { get; set; }
+        public decimal DailyPrice { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
