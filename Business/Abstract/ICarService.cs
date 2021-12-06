06fcad2f88a9e3ddb128c79b5a4b4281da0403c9
Nur SENKARA <nsenkara19@gmail.com>
2021-12-06 22:20:41 +0300

    Business katmanında IBrandService fonksiyonları tanımlandı.

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace Business.Abstract
 {
     public interface ICarService
     {
+        IResult Add(Car car);
+        IResult Update(Car car);
+        IResult Delete(Car car);
+        IDataResult<List<Car>> GetAll();
+        IDataResult<Car> GetByCarId(int carId);
+
     }
 }
---
+++ DataAccess/Concrete/EntityFramework/EfClaimDal.cs
@@ -0,0 +1,18 @@
+﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
+using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfClaimDal : EfEntityRepositoryBase<UserOperationClaim, RentACarContext>, IClaimDal
+    {
+       
+    }
+}
