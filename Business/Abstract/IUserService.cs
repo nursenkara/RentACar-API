0180875cbd4f45281aabbd9621e2e8d77fb86661
Nur SENKARA <nsenkara19@gmail.com>
2021-12-07 15:56:04 +0300

    IRentalservice,IUserService,ICustomerService IColorService business katmanında oluşturuldu.

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        void Add(User user);
+        void Update(User user);
+        List<OperationClaim> GetClaims(User user);
+        User GetByMail(string email);
+    }
+}
