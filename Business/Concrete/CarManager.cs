b248f962266619f4121f1497f2c5aa1fdc779729
Nur SENKARA <nsenkara19@gmail.com>
2021-12-07 17:17:56 +0300

    BrandManager dolduruldu ve business rules class ı oluşturuldu.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        public IResult Add(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Car>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<Car> GetByCarId(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string AddedMessage = "Entity Added!";
+        public static string DeletedMessage = "Entity Deleted!";
+        public static string EntitiesListed = "Entities Listed!";
+        public static string UpdatedMessage = "Entity Updated";
+        public static string ErrorMessage = "The data you have entered does not comply with the rules. Please try again!";
+
+    }
+}
