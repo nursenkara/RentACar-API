a25b51fdbaa83cced318dbba23600416192f5643
Nur SENKARA <nsenkara19@gmail.com>
2021-12-08 23:00:32 +0300

    Business/Concrete katmanına CustomerManager,UserManager,ColorManager eklendi. Core/Utilities/Helpers/FileHelper eklendi.

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,62 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        ICarImageDal _carImageDal;
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _carImageDal = carImageDal;
+        }
+        public IResult Add(CarImage carImage, IFormFile formFile)
+        {
+            _carImageDal.Add()
+        }
+
+        public IResult Delete(CarImage carImage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<CarImage>> GetByCarId(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<CarImage> GetById(int carImageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(CarImage carImage, IFormFile formFile)
+        {
+            throw new NotImplementedException();
+        }
+        private IResult CheckCarImageCount(int carId)
+        {
+            var result = _carImageDal.GetAll(c => c.CarId == carId);
+            if (result.Count > 4)
+            {
+                return new ErrorResult(Messages.ImageCount);
+            }
+
+            return new SuccessResult();
+        }
+    }
+}
